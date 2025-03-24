6b8ba4b1f23d88b10c2d119416007769630d598e
Vitalii Lipovetskii <vlipovetskii@users.noreply.github.com>
2025-03-24 17:52:12 +0200

    MimeType naming improvements

--- src/main/kotlin/vlite/core/domain/objects/MimeType.kt
+++ src/main/kotlin/vlite/core/domain/objects/MimeType.kt
@@ -34,21 +34,21 @@ public data class MimeType(val mimeContentType: ContentType, val contentSubType:
     @Suppress("unused")
     public companion object {
 
-        public val PLAIN: MimeType = MimeType(ContentType.TEXT, "plain")
-        public val HTML: MimeType = MimeType(ContentType.TEXT, "html")
-        public val XLS: MimeType = MimeType(ContentType.APPLICATION, "vnd.ms-excel")
-        public val XLSX: MimeType = MimeType(ContentType.APPLICATION, "vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        public val TEXT_PLAIN: MimeType = MimeType(MimeType.ContentType.TEXT, "plain")
+        public val TEXT_HTML: MimeType = MimeType(MimeType.ContentType.TEXT, "html")
+        public val XLS: MimeType = MimeType(MimeType.ContentType.APPLICATION, "vnd.ms-excel")
+        public val XLSX: MimeType = MimeType(MimeType.ContentType.APPLICATION, "vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
-        public val IMAGE: MimeType = MimeType(ContentType.IMAGE)
-        public val JPEG: MimeType = MimeType(ContentType.IMAGE, "jpeg")
+        public val IMAGE: MimeType = MimeType(MimeType.ContentType.IMAGE)
+        public val IMAGE_JPEG: MimeType = MimeType(MimeType.ContentType.IMAGE, "jpeg")
 
-        public val AUDIO: MimeType = MimeType(ContentType.AUDIO)
-        public val MP3: MimeType = MimeType(ContentType.AUDIO, "mpeg")
+        public val AUDIO: MimeType = MimeType(MimeType.ContentType.AUDIO)
+        public val AUDIO_MP3: MimeType = MimeType(MimeType.ContentType.AUDIO, "mpeg")
 
-        public val VIDEO: MimeType = MimeType(ContentType.VIDEO)
-        public val AVI: MimeType = MimeType(ContentType.VIDEO, "x-msvideo")
-        public val MPEG: MimeType = MimeType(ContentType.VIDEO, "mpeg")
-        public val MP4: MimeType = MimeType(ContentType.VIDEO, "mp4")
+        public val VIDEO: MimeType = MimeType(MimeType.ContentType.VIDEO)
+        public val VIDEO_AVI: MimeType = MimeType(MimeType.ContentType.VIDEO, "x-msvideo")
+        public val VIDEO_MPEG: MimeType = MimeType(MimeType.ContentType.VIDEO, "mpeg")
+        public val VIDEO_MP4: MimeType = MimeType(MimeType.ContentType.VIDEO, "mp4")
 
     }
 
